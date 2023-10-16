95b981bdd8b451dd31cc0ef6c7c220e6ce009160
Andrew <92659244+TheAndrewLa@users.noreply.github.com>
2023-10-16 11:34:02 +0700

    Updated 5th task

--- task_5.py
+++ task_5.py
@@ -1,15 +1,18 @@
 #!/bin/env python3
 
-def add(a, b):
-    return a + b
+def div(a, b):
+    return a / b
 
 
 def specialize(f, *args1, **kwargs1):
-    def specialized(*args2):
-        return f(*(args1 + args2), **kwargs1)
+    def specialized(*args2, **kwargs2):
+        kwargs2.update(kwargs1)
+        args2 += args1
+
+        return f(*args2, **kwargs2)
 
     return specialized
 
 
-inc = specialize(add, 1)
-print(inc(34))
+mult_reverse = specialize(div, a=1)
+print(mult_reverse(a=34, b=34)) # printing 0.0294... (not 1.0 because we set 'a' while specializing)
