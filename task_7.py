7e5d7553c8e0349a1bac41e5a70bd3d8e12d7bf1
Andrew <92659244+TheAndrewLa@users.noreply.github.com>
2023-10-18 15:56:26 +0700

    Updated 7th task

--- task_7.py
+++ task_7.py
@@ -1,19 +1,24 @@
 #!/bin/env python3
 
-def flatten(array: list[any], max_depth=1) -> list:
+def flatten(array: list[any], max_depth=None) -> list:
     flatten_list = []
+
+    def cmp_le(a, b):
+        if b is None:
+            return True
+
+        return a <= b
 
     def flat(sub_array: list[any], depth):
         for i in sub_array:
-            if isinstance(i, list) and depth <= max_depth:
+            if isinstance(i, list) and cmp_le(depth, max_depth):
                 flat(i, depth + 1)
             else:
                 flatten_list.append(i)
 
     flat(array, 1)
-
     return flatten_list
 
 
-print(flatten([1, 2, '3', [1, [[[[[2]]]]], 3], -65, False, [True, True, False]], 4))
+print(flatten([1, 2, '3', [1, [[[[[2]]]]], 3], -65, False, [True, True, False]]))
 # [1, 2, '3', 1, [[2]], 3, -65, False, True, True, False]
